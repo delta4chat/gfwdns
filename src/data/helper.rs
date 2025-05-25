a81b58e514b5f8534256d10b57b4603b82246c2c
Delta 4 <156182500+delta4chat@users.noreply.github.com>
2025-05-26 00:14:12 +0800

    no longer include {inside,outside}_ips.bin as static data

--- src/data/helper.rs
+++ src/data/helper.rs
@@ -34,3 +34,35 @@ pub const fn ip_split<const V4: usize, const V6: usize>(ips: &[Subnet])
 
     (ipv4, ipv6)
 }
+
+#[macro_export]
+macro_rules! ip_include {
+    ($file:literal) => {
+        // this "d" variable is needed for avoid include .bin to final output.
+        // only the length and parsed Subnet array will be included as static/const rodata.
+        pub const IPV4_LEN: usize = {
+            let d = include_bytes!($file);
+            u32::from_be_bytes([ d[0], d[1], d[2], d[3] ]) as usize
+            // "d" leave scope
+        };
+
+        pub const IPV6_LEN: usize = {
+            let d = include_bytes!($file);
+            u32::from_be_bytes([ d[4], d[5], d[6], d[7] ]) as usize
+            // "d" leave scope
+        };
+
+        pub const IP_LEN: usize = IPV4_LEN + IPV6_LEN;
+
+        pub static IP_LIST: [Subnet; IP_LEN] = {
+            let d = include_bytes!($file);
+            Subnet::parse_skip(d, IPV4_LEN, 8)
+            // "d" leave scope
+        };
+
+
+        static IP46: ([Subnet; IPV4_LEN], [Subnet; IPV6_LEN]) = ip_split(&IP_LIST);
+        pub const IPV4_LIST: &[Subnet; IPV4_LEN] = &IP46.0;
+        pub const IPV6_LIST: &[Subnet; IPV6_LEN] = &IP46.1;
+    }
+}
--- src/data/outside_ips.rs
+++ src/data/outside_ips.rs
@@ -2,15 +2,5 @@ use crate::*;
 
 /* DE+ET+EC+TT+IT: IPv4+IPv6 */
 
-static DATA: &[u8] = include_bytes!("outside_ips.bin");
-
-pub const IPV4_LEN: usize = u32::from_be_bytes([ DATA[0], DATA[1], DATA[2], DATA[3] ]) as usize;
-pub const IPV6_LEN: usize = u32::from_be_bytes([ DATA[4], DATA[5], DATA[6], DATA[7] ]) as usize;
-pub const IP_LEN: usize = IPV4_LEN + IPV6_LEN;
-
-pub static IP_LIST: [Subnet; IP_LEN] = Subnet::parse_skip(DATA, IPV4_LEN, 8);
-
-static IP46: ([Subnet; IPV4_LEN], [Subnet; IPV6_LEN]) = ip_split(&IP_LIST);
-pub const IPV4_LIST: &[Subnet; IPV4_LEN] = &IP46.0;
-pub const IPV6_LIST: &[Subnet; IPV6_LEN] = &IP46.1;
+ip_include!("outside_ips.bin");
 
